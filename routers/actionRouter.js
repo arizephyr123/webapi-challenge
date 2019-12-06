308e3a9eacb374101a169a99acfdd5afb277dd10
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-06 09:11:21 -0800

    file structure established, created, express server/Routers functional

---
+++ routers/actionRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const actionHelpers = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ routers/projectRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const projectHelpers = require('../data/helpers/projectModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
